5aa34d526236bdcaa21534f8b0968927cad5daa1
suhueon <degree.h0528@gmail.com>
2024-10-31 21:09:25 +0900

    로그인 세션  저장까지 완료

--- KOSAFIA/applicationLayer/gameapp/src/main/java/com/kosafia/gameapp/controllers/user/UserController.java
+++ KOSAFIA/applicationLayer/gameapp/src/main/java/com/kosafia/gameapp/controllers/user/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 import java.util.Map;
 
 @RestController
@@ -17,9 +20,31 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
-    // 로그인 엔드포인트
+    // // 로그인 엔드포인트
+    // @PostMapping("/login")
+    // public ResponseEntity<String> login(@RequestBody Map<String, String>
+    // loginData, HttpSession session) {
+    // String email = loginData.get("email");
+    // String password = loginData.get("password");
+
+    // // MySQL에서 사용자 조회
+    // User user = userMapper.findByEmail(email);
+
+    // // 사용자 인증
+    // if (user != null && user.getPassword().equals(password) && user.getStatus()
+    // == 1) {
+    // session.setAttribute("user", user); // 세션에 사용자 정보 저장
+    // return ResponseEntity.ok("로그인 성공");
+    // } else {
+    // // 로그인 실패 시 세션을 비워둠
+    // session.invalidate(); // 세션 초기화
+    // return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 실패: 이메일이나
+    // 비밀번호가 잘못되었습니다.");
+    // }
+    // }
+
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody Map<String, String> loginData, HttpSession session) {
+    public ResponseEntity<String> login(@RequestBody Map<String, String> loginData, HttpServletRequest request) {
         String email = loginData.get("email");
         String password = loginData.get("password");
 
@@ -28,7 +53,8 @@ public class UserController {
 
         // 사용자 인증
         if (user != null && user.getPassword().equals(password) && user.getStatus() == 1) {
-            session.setAttribute("user", user); // 세션에 사용자 정보 저장
+            HttpSession session = request.getSession(true); // 로그인 성공 시에만 세션 생성
+            session.setAttribute("user", user);
             return ResponseEntity.ok("로그인 성공");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 실패: 이메일이나 비밀번호가 잘못되었습니다.");
