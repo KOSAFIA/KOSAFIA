97a83ec66988880054c65b82afff9c8a76d006ad
Lee Haeun <kmbalhae07@naver.com>
2024-11-15 18:50:38 +0900

    성공함

--- KOSAFIA/applicationLayer/gameapp/src/main/java/com/kosafia/gameapp/services/game/GameServiceImpl.java
+++ KOSAFIA/applicationLayer/gameapp/src/main/java/com/kosafia/gameapp/services/game/GameServiceImpl.java
@@ -13,47 +13,55 @@ import org.springframework.stereotype.Service;
 import com.kosafia.gameapp.models.gameroom.GameStatus;
 import com.kosafia.gameapp.models.gameroom.Player;
 import com.kosafia.gameapp.models.gameroom.Role;
+import com.kosafia.gameapp.repositories.gameroom.RoomRepository;
 
 @Service
 public class GameServiceImpl implements GameService {
 
-    // @Override
-    // public void handleNightActions(Player player) {
-    // Player doctorTarget = null;
-    // Player mafiaTarget = null;
+    @Autowired
+    RoomRepository roomRepository;
 
-    // // 1. 마피아, 경찰, 의사의 행동을 정리
-    // if (player.getRole() == Role.MAFIA && player.getTarget() != null) {
-    // mafiaTarget = players.get(player.getTarget() - 1);
-    // }
+    @Override
+    public void handleNightActions(List<Player> players, Integer roomKey) {
+        Integer doctorTarget = null;
+        Integer mafiaTarget = null;
 
-    // if (player.getRole() == Role.DOCTOR && player.getTarget() != null) {
-    // doctorTarget = players.get(player.getTarget() - 1);
-    // }
+        for (Player player : players) {
+            // 1. 마피아, 경찰, 의사의 행동을 정리
+            if (player.getRole() == Role.MAFIA && player.getTarget() != null) {
+                mafiaTarget = player.getTarget();
+            }
 
-    // if (player.getRole() == Role.POLICE && player.getTarget() != null) {
-    // Player investigatedPlayer = players.get(player.getTarget() - 1);
-    // // 경찰은 마피아 여부를 조사하여 메시지로 알려줌
-    // boolean isMafia = investigatedPlayer.getRole() == Role.MAFIA;
-    // System.out.println(
-    // "경찰 조사 결과: " + investigatedPlayer.getPlayerNumber() + "은 "
-    // + (isMafia ? "마피아입니다." : "마피아가 아닙니다."));
-    // }
+            if (player.getRole() == Role.DOCTOR && player.getTarget() != null) {
+                doctorTarget = player.getTarget();
+            }
 
-    // // 2. 의사가 마피아의 타겟을 보호하는지 확인
-    // if (mafiaTarget != null && doctorTarget != null &&
-    // mafiaTarget.equals(doctorTarget)) {
-    // System.out.println("의사가 마피아의 타겟을 보호했습니다!");
-    // } else if (mafiaTarget != null) {
-    // // 마피아의 타겟이 보호받지 못했다면 사망 처리
-    // mafiaTarget.setAlive(false);
-    // System.out.println(mafiaTarget.getPlayerNumber() + "은(는) 마피아에게 살해당했습니다.");
-    // }
+            if (player.getRole() == Role.POLICE && player.getTarget() != null) {
+                Integer investigatedPlayer = player.getTarget();
+                // 경찰은 마피아 여부를 조사하여 메시지로 알려줌
+                boolean isMafia = (roomRepository.getRoom(roomKey).getPlayerByPlayerNumber(investigatedPlayer)
+                        .getRole() == Role.MAFIA);
+                System.out.println(
+                        "경찰 조사 결과: "
+                                + roomRepository.getRoom(roomKey).getPlayerByPlayerNumber(investigatedPlayer)
+                                        .getPlayerNumber()
+                                + "은 "
+                                + (isMafia ? "마피아입니다." : "마피아가 아닙니다."));
+            }
 
-    // // 플레이어의 상태가 제대로 변경되었는지 확인 (디버깅용)
-    // System.out.println("업데이트된 플레이어 상태: " + player.getPlayerNumber() + " - alive
-    // 여부 : " + player.isAlive());
-    // }
+            // 2. 의사가 마피아의 타겟을 보호하는지 확인
+            if (mafiaTarget != null && doctorTarget != null &&
+                    mafiaTarget.equals(doctorTarget)) {
+                System.out.println("의사가 마피아의 타겟을 보호했습니다!");
+            } else if (mafiaTarget != null) {
+                // 마피아의 타겟이 보호받지 못했다면 사망 처리
+                roomRepository.getRoom(roomKey).getPlayerByPlayerNumber(mafiaTarget).setAlive(false);
+                System.out
+                        .println(roomRepository.getRoom(roomKey).getPlayerByPlayerNumber(mafiaTarget).getPlayerNumber()
+                                + "은(는) 마피아에게 살해당했습니다.");
+            }
+        }
+    }
 
     // ===============김남영 추가=============
     @Autowired
